4775d73b86638a7319c915588319b24b51ee1859
grace-media-web <gracemediaweb@gmail.com>
2026-05-14 10:33:35 +0530

    Update App.tsx
    Spelling correction

--- src/App.tsx
+++ src/App.tsx
@@ -359,7 +359,7 @@ export default function App() {
                 <Users size={24} />
               </div>
               <h3 className="text-2xl font-bold mb-2">27,000+ Children</h3>
-              <p className="text-white/70">வளர்  தலைமுறையை இயேசுவுக்காய் ஆயத்தப்படுத்துவது எங்கள் நஎங்களின் பிரதான நோக்கம்</p>
+              <p className="text-white/70">வளர்  தலைமுறையை இயேசுவுக்காய் ஆயத்தப்படுத்துவது எங்களின் பிரதான நோக்கம்</p>
             </div>
             <div className="bg-white/10 backdrop-blur-md p-8 rounded-3xl border border-white/10">
               <div className="bg-yellow-400 text-blue-900 w-12 h-12 rounded-xl flex items-center justify-center mb-6">
